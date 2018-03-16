6f946738e87bbf6d61428db9ee1e1f11ec9c65b0
Lucas Craveiro <lucashcraveiro@gmail.com>
2018-03-16 14:37:58 -0700

    Add WhiteNoise class

---
+++ app/src/main/java/com/example/lucascraveiro/babystuff/WhiteNoiseGrid.java
@@ -0,0 +1,17 @@
+package com.example.lucascraveiro.babystuff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WhiteNoiseGrid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_white_noise_grid);
+    }
+
+    public void Backpage(View view) {
+    }
+}
